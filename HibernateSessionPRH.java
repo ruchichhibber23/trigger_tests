735fdce342f372b55aba20ee35e369fa296fb9c0
ruchi <ruchi.chhibber@gmail.com>
2016-11-29 16:25:05 +0530

    test

--- HibernateSessionPRH.java
+++ HibernateSessionPRH.java
@@ -5,26 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/*
- Rule:
- <Rule id="CIGITAL-JAVA-HIBERNATE-PRH" lang="java">
- <Category>Hibernate Security</Category>
- <Title>Session not closed in finally block</Title>
- <Description>
- Checks that a session was closed in a finally block and not
- the try or catch block.
- </Description>
- <Match block="finally">
- <QualifiedName>org.hibernate.Session\b</QualifiedName>
- <Method>close</Method>
- </Match>
- <Standards>
- <Standard file="resource-handling.xml">
- <Context>Hibernate</Context>
- </Standard>
- </Standards>
- </Rule>
- */
 public class HibernateSessionPRH {
 	HttpServletRequest request = null;
 
